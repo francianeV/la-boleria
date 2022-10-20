6ba748d079c4cd23882be2481f3f71165bd9237d
Franciane <franciane.penina@gmail.com>
2022-10-20 01:58:02 -0300

    initializing project

---
+++ src/server.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const server = express();
+server.use(cors());
+server.use(express.json());
+
+server.listen(process.env.PORT, () => console.log(`server listening on Port ${process.env.PORT}`));
